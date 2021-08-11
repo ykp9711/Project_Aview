396788ef71f2378bcd214a979278dd0ec75422e6
ykp <ykp@DESKTOP-ASCLKVL>
2021-08-11 19:35:00 +0900

    파일 업로드 메소드화

--- src/main/java/com/Aview/controller/BoardController.java
+++ src/main/java/com/Aview/controller/BoardController.java
@@ -1,7 +1,9 @@
 package com.Aview.controller;
 
 import java.io.File;
+import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
 import java.util.UUID;
 
 import javax.servlet.http.HttpServletRequest;
@@ -19,9 +21,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.Aview.domain.AcademyBoardVO;
+import com.Aview.domain.AcademyPhotoVO;
 import com.Aview.domain.AcademyTeacherVO;
 import com.Aview.domain.Criteria;
 import com.Aview.domain.PageDTO;
+import com.Aview.domain.Upload;
 import com.Aview.mapper.BoardMapper;
 
 import lombok.extern.log4j.Log4j;
@@ -31,7 +35,9 @@ import lombok.extern.log4j.Log4j;
 @Log4j
 @RequestMapping("/board/*")
 public class BoardController {
-   
+   @Autowired
+   Upload up;
+	
    @Autowired
    BoardMapper mapper;
    
@@ -49,33 +55,13 @@ public class BoardController {
    
    @PostMapping("/register")
    public String BoardRegister(MultipartFile[] file, @ModelAttribute AcademyBoardVO av, HttpServletResponse resp, HttpSession session ,  HttpServletRequest req) throws Exception{
-	   //String uploadPath = "C:\\Users\\ykp\\git\\Project_Aview\\src\\main\\webapp\\resources\\FileImage"; // 경필 저장 경로
-	   String uploadPath = "C:\\Users\\YJG\\git\\Project_Aview\\src\\main\\webapp\\resources\\FileImage"; //정근 저장 주소
-	  
-	   File target = new File(uploadPath);
-       if(!target.exists()) target.mkdirs(); // 파일 경로에 폴더 없으면 새로운 폴더 생성
-       
-       for(int i=0; i<file.length; i++) { // 파일 갯수만큼 반복
-      	 
-           String orgFileName = file[i].getOriginalFilename();  //파일 실제이름 
-           String orgFileExtension = orgFileName.substring(orgFileName.lastIndexOf(".")); // 파일 확장자 exe같은거
-           String saveFileName = UUID.randomUUID().toString().replaceAll("-", "") + orgFileExtension; // 파일 랜덤이름
-           Long saveFileSize = file[i].getSize();
-           
-           log.info("================== file start ==================");
-           log.info("파일 실제 이름: "+orgFileName);
-           log.info("파일 저장 이름: "+saveFileName);
-           log.info("파일 크기: "+saveFileSize);
-           log.info("content type: "+file[i].getContentType());
-           
-           av.setAcademyPhoto(saveFileName);
-
-           target = new File(uploadPath, saveFileName);
-           file[i].transferTo(target);
-
-           	av.setAcademyPhoto(saveFileName);
-			/* av.setAcademyMapPhoto(saveFileName); */
-       }
+	   ArrayList<String> fileName = new ArrayList<>(); // 파일 이름들을 받을 리스트 생성
+	   
+	   fileName=(ArrayList<String>) up.FileUpload(file); // 받아온 파일 이름들을 리스트에 저장
+	   
+	   for(int i=0; i<fileName.size(); i++) { // 리스트 크기만큼 반복
+		   av.setAcademyPhoto(fileName.get(i)); // 파일들을 DB에 저장
+	   }
 	   
       av.setAcademyId((String)session.getAttribute("session_id"));
       av.setAcademyIntro("임시값"); // intro는 후에 사용하게되면 이 부분 지우고 사용
@@ -103,33 +89,7 @@ public class BoardController {
    
    @PostMapping("/registerTeacher")
    public String BoardRegisterTeacher(MultipartFile[] file, @ModelAttribute AcademyTeacherVO tv, HttpServletResponse resp, HttpSession session ,  HttpServletRequest req) throws Exception{
-	   //String uploadPath = "C:\\Users\\ykp\\git\\Project_Aview\\src\\main\\webapp\\resources\\FileImage"; // 경필 저장 경로
-	   String uploadPath = "C:\\Users\\YJG\\git\\Project_Aview\\src\\main\\webapp\\resources\\FileImage"; //정근 저장 주소
-	  
-	   File target = new File(uploadPath);
-       if(!target.exists()) target.mkdirs(); // 파일 경로에 폴더 없으면 새로운 폴더 생성
-       
-       for(int i=0; i<file.length; i++) { // 파일 갯수만큼 반복
-      	 
-           String orgFileName = file[i].getOriginalFilename();  //파일 실제이름 
-           String orgFileExtension = orgFileName.substring(orgFileName.lastIndexOf(".")); // 파일 확장자 exe같은거
-           String saveFileName = UUID.randomUUID().toString().replaceAll("-", "") + orgFileExtension; // 파일 랜덤이름
-           Long saveFileSize = file[i].getSize();
-           
-           log.info("================== file start ==================");
-           log.info("파일 실제 이름: "+orgFileName);
-           log.info("파일 저장 이름: "+saveFileName);
-           log.info("파일 크기: "+saveFileSize);
-           log.info("content type: "+file[i].getContentType());
-           
-           tv.setTeacherPhoto1(saveFileName);
-
-           target = new File(uploadPath, saveFileName);
-           file[i].transferTo(target);
-
-           	tv.setTeacherPhoto1(saveFileName);
-			/* av.setAcademyMapPhoto(saveFileName); */
-       }
+	   
 	   
       log.info(tv);
       resp.setCharacterEncoding("UTF-8");
@@ -230,8 +190,15 @@ public class BoardController {
 	   
    }
    
-   @GetMapping("/test")
-   public String test() {
-	   return "/WebContent/app/academy/test";
-   }
+   @PostMapping("/acatest")
+   public void acatest(MultipartFile file[], AcademyPhotoVO vo) throws IOException {
+	   ArrayList<String> fileName = new ArrayList<>(); // 리턴값을 받아 줄 리스트 생성
+	  log.info("여기-----");
+		  fileName = (ArrayList<String>) up.FileUpload(file); // 저장된 파일이름 받아줌
+	
+		  log.info(fileName.get(0));
+
+	   
+		   }
+   
 }
